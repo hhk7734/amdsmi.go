31ef98e833eccbb361e2369f45865dbe7a2f8727
Hyeonki Hong <hhk7734@gmail.com>
2024-05-23 00:53:47 +0900

    feat: add FanSpeed and BusyPercent

--- pkg/rsmi/rsmi_wrapper.go
+++ pkg/rsmi/rsmi_wrapper.go
@@ -77,6 +77,24 @@ func (r *RSMI) DevMemoryUsage(deviceIndex uint32, type_ memoryType) (uint64, err
 	return usage, nil
 }
 
+func (r *RSMI) DevFanSpeed(deviceIndex uint32, fanIndex uint32) (int64, error) {
+	var fanSpeed int64
+	if err := rsmi_dev_fan_speed_get(deviceIndex, fanIndex, &fanSpeed).Err(); err != nil {
+		return 0, err
+	}
+
+	return fanSpeed, nil
+}
+
+func (r *RSMI) DevFanSpeedMax(deviceIndex uint32, fanIndex uint32) (uint64, error) {
+	var maxSpeed uint64
+	if err := rsmi_dev_fan_speed_max_get(deviceIndex, fanIndex, &maxSpeed).Err(); err != nil {
+		return 0, err
+	}
+
+	return maxSpeed, nil
+}
+
 func (r *RSMI) DevTempMetric(deviceIndex uint32, type_ temperatureType, metric temperatureMetric) (float64, error) {
 	var temp int64
 	if err := rsmi_dev_temp_metric_get(deviceIndex, uint32(type_), temperatureMetric(metric), &temp).Err(); err != nil {
@@ -84,4 +102,13 @@ func (r *RSMI) DevTempMetric(deviceIndex uint32, type_ temperatureType, metric t
 	}
 
 	return float64(temp) / 1000, nil
+}
+
+func (r *RSMI) DevBusyPercent(deviceIndex uint32) (float64, error) {
+	var busyPercent uint32
+	if err := rsmi_dev_busy_percent_get(deviceIndex, &busyPercent).Err(); err != nil {
+		return 0, err
+	}
+
+	return float64(busyPercent), nil
 }
